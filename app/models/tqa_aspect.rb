a8503d859733460dc669001cc6b657dbf954c0e7
alecwesterman <alecwesterman@gmail.com>
2015-01-20 13:49:24 -0700

    adjusted bar for multi data

--- app/models/tqa_aspect.rb
+++ app/models/tqa_aspect.rb
@@ -5,4 +5,14 @@ class TqaAspect < ActiveRecord::Base
   validates :percent_run, numericality: { less_than_or_equal_to: 100, greater_than_or_equal_to: 0 }
   validates :percent_pass, numericality: { less_than_or_equal_to: 100, greater_than_or_equal_to: 0 }
   validates :tqa_category_id, presence: true
+
+  def portion_failed_testing
+    100-portion_passed_testing-portion_to_be_tested
+  end
+  def portion_passed_testing
+    (self.percent_run)*(self.percent_pass)/100
+  end
+  def portion_to_be_tested
+    100-self.percent_run
+  end
 end
